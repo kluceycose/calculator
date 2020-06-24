1ba390c5e175bb7e8dffc3ead6d28edfcb3010b2
Ken Lucey-Cose <kluceycose@gmail.com>
2020-06-24 14:05:55 -0700

    Updated testing to work with ES6 modules using Jest

---
+++ README.md
@@ -0,0 +1,9 @@
+# Calculator
+A basic, JavaScript-based calculator, modeled after the Ubuntu basic calculator
+
+## Demonstrated Skills
++ Abstraction via import/export
++ DOM manipulation
++ Keyboard support
++ Flex and Grid organization
++ ES6 Unit testing with Jest
--- math.test.js
+++ math.test.js
@@ -1,60 +1,61 @@
-const calculator = require('./math.js');
+//const calculator = require('./math.js');
+import {add, subtract, multiply, divide, mod} from './math.js';
 
 describe('add', function() {
     it('adds positive numbers', function() {
-        expect(calculator.add(4,6)).toEqual(10);
+        expect(+(add(4,6))).toBe(10);
     });
 
     it('adds large positive numbers', function() {
-        expect(calculator.add(26584, 159874)).toEqual(186458);
+        expect(+(add(26584, 159874))).toEqual(186458);
     });
 });
 
 describe('subtract', function() {
     it('subtracts positive numbers', function() {
-        expect(calculator.subtract(10, 4)).toEqual(6);
+        expect(+(subtract(10, 4))).toEqual(6);
     });
 });
 
 describe('multiply', function() {
     it('multiplies two positive numbers', function() {
-        expect(calculator.multiply(2, 4)).toEqual(8);
+        expect(+(multiply(2, 4))).toEqual(8);
     });
 });
 
 describe('divide', function() {
     it('divides two non-zero numbers', function() {
-        expect(calculator.divide(10, 2)).toEqual(5);
+        expect(+(divide(10, 2))).toEqual(5);
     });
 
     it('returns 0 when dividing 0 by a number', function() {
-        expect(calculator.divide(0, 5)).toEqual(0);
+        expect(+(divide(0, 5))).toEqual(0);
     });
 
-    it('returns undefined when dividing by 0', function() {
-        expect(calculator.divide(5, 0)).toEqual(undefined);
+    it('returns snarky comment when dividing by 0', function() {
+        expect(divide(5, 0)).toEqual("Trying to end the universe, are we?");
     });
 
     it('returns NaN when both a and b are 0', function() {
-        expect(calculator.divide(0,0)).toEqual("NaN");
+        expect(divide(0,0)).toEqual("NaN");
     });
 });
 
 describe('mod', function() {
     it('returns the remainder after division', function() {
-        expect(calculator.mod(10, 3)).toEqual(1);
+        expect(+(mod(10, 3))).toEqual(1);
     });
 
     it('returns 0 when a is 0', function() {
-        expect(calculator.mod(0, 5)).toEqual(0);
+        expect(+(mod(0, 5))).toEqual(0);
     });
 
-    it('returns undefined when b is 0', function() {
-        expect(calculator.divide(5, 0)).toEqual(undefined);
+    it('returns snarky comment when b is 0', function() {
+        expect(mod(5, 0)).toEqual("Trying to end the universe, are we?");
     });
 
     it('returns NaN when both a and b are 0', function() {
-        expect(calculator.divide(0,0)).toEqual("NaN");
+        expect(mod(0,0)).toEqual("NaN");
     });
 });
 
